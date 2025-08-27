192f8e25df3080bb791244aefb13f16018f52dec
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2025-08-27 10:30:50 +0900

    :bug:Fix JpaRepository 상속 누락 수정

--- src/main/java/com/mathfusion/domain/user/repository/SocialUserRepository.java
+++ src/main/java/com/mathfusion/domain/user/repository/SocialUserRepository.java
@@ -2,11 +2,10 @@ package com.mathfusion.domain.user.repository;
 
 import com.mathfusion.domain.user.entity.User;
 import com.mathfusion.domain.user.entity.enums.LoginType;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-@Repository
-public interface SocialUserRepository {
+public interface SocialUserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySocialIdAndLoginType(String socialId, LoginType loginType);
 }
