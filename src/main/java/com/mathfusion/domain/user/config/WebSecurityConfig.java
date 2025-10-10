7dc00d21d47b27c290ec90213e1585e6b3714e78
techncherry <technarchitect@gmail.com>
2025-10-10 12:25:49 +0900

    🚀 Deploy: 도메인 변경해서 배포

--- src/main/java/com/mathfusion/domain/user/config/WebSecurityConfig.java
+++ src/main/java/com/mathfusion/domain/user/config/WebSecurityConfig.java
@@ -82,6 +82,7 @@ public class WebSecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "https://topmapi.duckdns.org",
+                "https://tophatmapi.duckdns.org",
                 "https://ds-tophat.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
