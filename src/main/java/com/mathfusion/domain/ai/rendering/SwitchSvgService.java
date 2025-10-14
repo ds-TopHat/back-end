75a79b7756034a96ec9947f2f31606eb7301f453
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2025-10-15 03:33:27 +0900

    :bug:Fix Rollback2

--- src/main/java/com/mathfusion/domain/ai/rendering/SwitchSvgService.java
+++ src/main/java/com/mathfusion/domain/ai/rendering/SwitchSvgService.java
@@ -58,7 +58,10 @@ public class SwitchSvgService {
     private String renderLatexToSvg(String latex){
         try{
             // implementation 'org.scilab.forge:jlatexmath:1.0.7'
-            var formula = new org.scilab.forge.jlatexmath.TeXFormula(latex);
+            var formula = new org.scilab.forge.jlatexmath.TeXFormula(
+                    "\\usepackage{amsmath}\n" +
+                    "\\usepackage{amssymb}\n" +
+                            latex);
             var icon = formula.createTeXIcon(org.scilab.forge.jlatexmath.TeXConstants.STYLE_DISPLAY, 20);
             icon.setInsets(new Insets(2,2,2,2));
 
@@ -73,7 +76,15 @@ public class SwitchSvgService {
 
             StringWriter sw = new StringWriter();
             svgGen.stream(sw, true);
-            return sw.toString();
+            sw.flush();
+
+            String svg = sw.toString()
+                    .replace("font-family:'cmr10'", "font-family:'Noto Sans', sans-serif")
+                    .replace("font-family:'cmmi10'", "font-family:'Noto Sans', sans-serif")
+                    .replace("font-family:'cmsy10'", "font-family:'Noto Sans', sans-serif")
+                    .replace("font-family:'cmex10'", "font-family:'Noto Sans', sans-serif");
+
+            return svg;
         }catch(Exception e){
             return "<svg xmlns=\"http://www.w3.org/2000/svg\"><text>" + latex + "</text></svg>";
         }
